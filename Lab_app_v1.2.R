dfb6d9fee4cde4ab5ae668c0abb34db036457a07
Houston Saxe <hsaxe@ucdavis.edu>
2022-02-23 23:15:24 -0800

    Major update to app. More user friendly and flexible. Requires just expression data frame. Optional metadata.

--- Lab_app_v1.2.R
+++ Lab_app_v1.2.R
@@ -35,16 +35,16 @@ ui = dashboardPage(
                     inputPanel(
                         
                         box(
-                            fileInput('Data', 'Choose CSV file in plotting format (GeneID, ProteinID, metabolite, samples, expression level, etc. must all be in their own column)',
+                            fileInput('Data', 'Choose CSV file in plotting format (GeneID, ProteinID, metabolite, samples, etc. must all be in their own column)',
                                       accept = 'text/csv'),
+                            
+                            selectInput('metaquestion', 'Do you have metadata?', c('No', 'Yes')),
                             
                             fileInput('metadata', 'Choose metadata file',
-                                      accept = 'text/csv'),
-                            
-                            selectInput('PlotType', 'Select plot type:', c('Boxplot', 'Barplot')),
-                            width = 20),
+                                      accept = 'text/csv'), width = 20),
                         
                         box(
+                            selectInput('PlotType', 'Select plot type:', c('Boxplot', 'Barplot')),
                             selectInput('feat', 'What do you want to plot?', choices = NULL), width = 12),
                         
                         box(
@@ -59,30 +59,32 @@ ui = dashboardPage(
                         
                         box(
                             selectInput('y', 'Select plot y-axis:', choices = NULL),
-                            
+
                             selectizeInput('ysub', 'Subset y-axis (optional):', choices = NULL),
-                            
+
                             width = 12),
-                        
-                        box(
-                            selectInput('datsub', 'Subset Dataset (optional):', choices = NULL),
-                            
-                            selectizeInput('datsubfeat', 'Which feature(s)?', choices = NULL),
-                            
-                            width = 20),
                         
                         box(
                             selectInput('facet', 'Facet by:', choices = NULL),
                             
                             selectizeInput('fill', 'Fill by:', choices = NULL),
-                            width = 12)
-                    ),
-                    
+                            width = 12),
+
+                        box(
+                            selectInput('datsub', 'Subset Dataset (optional):', choices = NULL),
+
+                            selectizeInput('datsubfeat', 'Which feature(s)?', choices = NULL, multiple = T),
+
+                            width = 20)
+                        
+                    )
+                    ,
+
                     mainPanel(
                         plotOutput('Bplot')),
-                    
+
                     downloadButton("downloadData", "Download This Plot"),
-                    
+
                     box(
                         numericInput('SaveWidth', 'Change size of saved plot', value = 8, width = '100px'),
                         width = 1)
@@ -97,20 +99,24 @@ ui = dashboardPage(
 
 # Define server logic 
 server <- function(input, output, session) {
+    
     ## This defines the dataset to be worked on in the server based on user selection
     dataset <- reactive({
-        
+
         req(input$Data)
-        
-        fread(input$Data$datapath) 
+
+        fread(input$Data$datapath)
     })
     
+   
+    
     metadata <- reactive({
-        
+
         req(input$metadata)
-        
-        fread(input$metadata$datapath) 
+
+        fread(input$metadata$datapath)
     })
+    
     ## Each of these 'observe' functions with 'updateselect(ize)Input' is required to make input options based on previous user input. As you may notice, they are matched to ui inputs.
     observe({
         updateSelectInput(session, 'feat', 'Select ID name (GeneID, ProteinID, Metabolite, etc.)', choices = names(dataset()))
@@ -120,119 +126,158 @@ server <- function(input, output, session) {
         updateSelectizeInput(session, 'filter', 'Which feature(s)? If multiple, you should facet by variable selected above.', choices = unique(dataset()[[input$feat]]), server = T)
     })
     
+    ## Filtering the data based on user inputs
+            dataset02 = reactive({
+                
+                req(c(input$feat, input$filter))
+                
+                dataset() %>% 
+                    filter(.data[[input$feat]] %in% input$filter) %>%  
+                    pivot_longer(!.data[[input$feat]], names_to = 'Sample') %>%
+                    mutate(Group = gsub('..$', '', Sample))
+                
+            })
+                
+                dataset2 = reactive({
+                    
+                    req(c(input$Data, input$metadata))
+                    
+                    if(input$metaquestion == 'Yes') {
+                        
+                        dataset02() %>% 
+                            left_join(metadata())
+                        
+                    } else {
+                        
+                        dataset02()
+                    }
+                    
+                    
+                })
+
+        
+    ## Each of these 'observe' functions with 'updateselect(ize)Input' is required to make input options based on previous user input. As you may notice, they are matched to ui inputs.    
+    
     observe({
-        updateSelectizeInput(session, 'x', 'Select plot x-axis:', choices = names(dataset()), server = T)
+        updateSelectizeInput(session, 'x', 'Select plot x-axis:', choices = names(dataset2()), server = T)
     })
     
     observe({
-        updateSelectizeInput(session, 'xsub', 'Subset x-axis (optional):', choices = c('None', unique(dataset()[[input$x]])), selected = 'None', server = T)
+        updateSelectizeInput(session, 'xsub', 'Subset x-axis (optional):', choices = c('None', unique(dataset2()[[input$x]])), selected = 'None', server = T)
     })
     
     observe({
-        updateSelectizeInput(session, 'y', 'Select plot y-axis:', choices = names(dataset()), server = T)
+        updateSelectizeInput(session, 'y', 'Select plot y-axis:', choices = names(dataset2()), server = T)
+    })
+
+    observe({
+        updateSelectizeInput(session, 'ysub', 'Subset y-axis (optional):', choices = c('None', unique(dataset2()[[input$y]])), selected = 'None', server = T)
     })
     
     observe({
-        updateSelectizeInput(session, 'ysub', 'Subset y-axis (optional):', choices = c('None', unique(dataset()[[input$y]])), selected = 'None', server = T)
+        updateSelectizeInput(session, 'facet', 'Facet by:', choices = c('None', names(dataset2())), selected = 'None', server = T)
     })
     
     observe({
-        updateSelectizeInput(session, 'datsub', 'Subset Dataset (optional):', choices = c('None', names(dataset())), selected = 'None', server = T)
+        updateSelectizeInput(session, 'fill', 'Fill by:', choices = c(names(dataset2())), server = T)
+    })
+
+    observe({
+        updateSelectizeInput(session, 'datsub', 'Subset Dataset (optional):', choices = c('None', names(dataset2())), selected = 'None', server = T)
+    })
+
+    observe({
+        updateSelectizeInput(session, 'datsubfeat', 'Which feature(s)?', choices = c('None', unique(dataset2()[[input$datsub]])), selected = 'None', server = T)
+        
     })
     
-    observe({
-        updateSelectizeInput(session, 'datsubfeat', 'Which feature(s)?', choices = c('None', unique(dataset()[[input$datsub]])), selected = 'None', server = T)
+    ## Filtering the data based on user inputs
+    dataset3 = reactive({
+        
+        req(c(input$datsub, input$datsubfeat))
+        
+        dataset2() %>% 
+            filter(.data[[input$datsub]] %in% input$datsubfeat)
+        
     })
+
     
-    observe({
-        updateSelectizeInput(session, 'facet', 'Facet by:', choices = c('None', names(dataset())), selected = 'None', server = T)
-    })
-    
-    observe({
-        updateSelectizeInput(session, 'fill', 'Fill by:', choices = c(names(dataset())), server = T)
-    })
     ## Define plotting variable in server to be displayed in ui based on inputs
     plotInput = reactive({
-        ## Filtering the data based on user inputs
-        if(is.null(metadata())) {
-            
-            dat = dat %>% 
-                filter(get(input$feat) %in% input$filter) %>% 
-                pivot_longer(!GeneID, names_to = 'Samples') %>% 
-                mutate(Group = gsub('..$', '', Samples))
-            
-        } else {
-            
-            dat = dat %>% 
-                filter(get(input$feat) %in% input$filter) %>% 
-                pivot_longer(!GeneID, names_to = 'Samples') %>% 
-                mutate(Group = gsub('..$', '', Samples))
-        }
-        
-        
-        if(input$xsub != 'None') {
-            dat = dat %>% filter(get(input$x) %in% input$xsub)
-        }
-        
-        if(input$ysub != 'None') {
-            dat = dat %>% filter(get(input$y) %in% input$ysub)
-        }
         
         if(input$datsub != 'None') {
-            dat = dat %>% filter(get(input$datsub) %in% input$datsubfeat)
+            
+            dat = dataset3()
+        
+        } else {
+            
+            dat = dataset2()
         }
-        ## Conditional statement for input boxplot   
+
+        # if(input$xsub != 'None') {
+        #     dat = dat %>% filter(.data[[input$x]] %in% input$xsub)
+        # }
+        # 
+        # if(input$ysub != 'None') {
+        #     dat = dat %>% filter(.data[[input$y]] %in% input$ysub)
+        # }
+        # 
+        # if(input$datsub != 'None') {
+        #     dat = dat %>% filter(.data[[input$datsub]] %in% input$datsubfeat)
+        # }
+        ## Conditional statement for input boxplot
         if(input$PlotType == 'Boxplot') {
             if(input$facet == 'None') {
-                p = ggplot(dat, aes(reorder(get(input$x), get(input$y)), 
-                                    get(input$y), fill = get(input$fill)))+
+                p = ggplot(dat, aes(reorder(.data[[input$x]], .data[[input$y]]),
+                                    .data[[input$y]], fill = .data[[input$fill]]))+
                     geom_boxplot()+
                     scale_x_reordered()+
                     theme(text = element_text(size = 20), axis.text.x = element_text(angle = 30, vjust = 0.5))+
                     guides(fill = guide_legend(title = input$fill))+
+                    ggtitle(input$filter)+
                     labs(x = input$x, y = input$y)
-                
+
             } else {
-                
-                p = ggplot(dat, aes(reorder_within(get(input$x), get(input$y), get(input$facet)), 
-                                    get(input$y), fill = get(input$fill)))+
+
+                p = ggplot(dat, aes(reorder_within(.data[[input$x]], .data[[input$y]], .data[[input$facet]]),
+                                         .data[[input$y]], fill = .data[[input$fill]]))+
                     geom_boxplot()+
                     scale_x_reordered()+
                     theme(text = element_text(size = 20), axis.text.x = element_text(angle = 30, vjust = 0.5))+
                     guides(fill = guide_legend(title = input$fill))+
                     labs(x = input$x, y = input$y)+
-                    facet_wrap(~get(input$facet), scales = "free", ncol = 2)
+                    facet_wrap(~.data[[input$facet]], scales = "free", ncol = 2)
             }
-            
+
         } else {
             ## conditional input for input barplot
             if(input$PlotType == 'Barplot') {
                 if(input$facet == 'None') {
-                    p = ggplot(dat, aes(reorder(get(input$x), get(input$y)), 
-                                        get(input$y), fill = get(input$fill)))+
+                    p = ggplot(dat, aes(reorder(.data[[input$x]], .data[[input$y]]),
+                                        .data[[input$y]], fill = .data[[input$fill]]))+
                         geom_bar(stat = 'identity')+
                         scale_x_reordered()+
                         theme(text = element_text(size = 20), axis.text.x = element_text(angle = 30, vjust = 0.5))+
                         guides(fill = guide_legend(title = input$fill))+
                         labs(x = input$x, y = input$y)
-                    
+
                 } else {
-                    
-                    p = ggplot(dat, aes(reorder_within(get(input$x), get(input$y), get(input$facet)), 
-                                        get(input$y), fill = get(input$fill)))+
+
+                    p = ggplot(dat, aes(reorder_within(.data[[input$x]], .data[[input$y]], .data[[input$facet]]),
+                                        .data[[input$y]], fill = .data[[input$fill]]))+
                         geom_bar(stat = 'identity')+
                         scale_x_reordered()+
                         theme(text = element_text(size = 20), axis.text.x = element_text(angle = 30, vjust = 0.5))+
                         guides(fill = guide_legend(title = input$fill))+
                         labs(x = input$x, y = input$y)+
-                        facet_wrap(~get(input$facet), scales = "free_x")
+                        facet_wrap(~.data[[input$facet]], scales = "free_x")
                 }
-                
+
             }
-            
+
         }
-        
-        
+
+
     })
     ## This is required to set the plot to a variable of 'output'
     output$Bplot <- renderPlot({
@@ -242,7 +287,7 @@ server <- function(input, output, session) {
     output$downloadData <- downloadHandler(
         filename = function() { paste(gsub('[[:punct:]]', '_', input$filter), '.png', sep='_') },
         content = function(file) {
-            save_plot(file,plotInput(), base_height = input$SaveWidth)
+            save_plot(file, plotInput(), base_height = input$SaveWidth)
         }
     )
     
